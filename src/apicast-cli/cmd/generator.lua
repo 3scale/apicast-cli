c4f0839f8711e96ef711cdb5204371c92b7a70e5
Hallelujah <hery@rails-royce.org>
2017-05-16 18:17:03 +0200

    Ability to copy blank-app directory to new project

--- src/apicast-cli/cmd/generator.lua
+++ src/apicast-cli/cmd/generator.lua
@@ -1,14 +1,18 @@
-local lfs = require 'lfs'
+local utils_path = require "apicast-cli.utils.path"
+local path = require "pl.path"
+local dir = require "pl.dir"
 
 local create_new_project = function (args, _)
   local opts = args or {}
-  local name = string.gsub((opts.name or 'new-app'):lower(), ' ', '_')
-  local current_dir = lfs.currentdir()
-  local destiny = opts.path or current_dir
+  local name = opts.name or 'new-app'
+  name = string.gsub(name:lower(), ' ', '-')
+  local destiny = opts.path or path.currentdir()
 
-  local raw_app = current_dir .. '/blank-app'
-  local new_app = destiny .. "/" .. name
-  assert(os.execute("cp -a '" .. raw_app .. "' '" .. new_app .. "'"))
+  local raw_app = path.join(utils_path.find(), 'apicast-cli/blank-app')
+  local new_app = path.join(destiny, name)
+  local command = {"cp", "-a", raw_app, new_app}
+  print('Generating new project into ' .. new_app)
+  assert(os.execute(table.concat(command, " ")))
   print('done!')
   os.exit(0)
 end
--- src/apicast-cli/utils/path.lua
+++ src/apicast-cli/utils/path.lua
@@ -1,12 +1,12 @@
 local path = require "pl.path"
 local loader = require "luarocks.loader"
+local binding = require "resty.repl"
 
 local find_rock_directory = function()
   local file_path = loader.which("apicast-cli.utils.path")
   local f, err = file_path and io.open(file_path)
 
   if not f then
-    print("No such file " .. file_path)
     return nil, err
   else
     f:close()
@@ -17,21 +17,18 @@ end
 local find_dev_directory = function()
   -- If we run the script apicast-cli
   -- Look for the stacktrace and the file where the function was called
-  local package_path = ((debug.getinfo(1).source):match("^@?(.-)/apicast-cli$"))
-  local f = package_path and io.open(package_path.."../src/apicast-cli/utils/path.lua", 'r')
+  local package_path = (debug.getinfo(1).source):match("^@?(.-)/apicast%-cli/utils/path%.lua$")
+  local f = package_path and io.open(path.join(package_path, "apicast-cli/utils/path.lua"), 'r')
+
   if f then
     f:close()
-    if package_path == '.' then
-      return path.join(path.currentdir(),"src")
-    end
+    return package_path
   end
 end
 
 local find = function()
   return find_dev_directory() or find_rock_directory()
 end
-
-print(find_rock_directory())
 
 return {
   find = find
