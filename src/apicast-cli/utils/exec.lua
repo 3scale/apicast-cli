628fa0cc74d18ec797ad7ffaf7062e1bc91ae323
Michal Cichra <michal@cichra.cz>
2017-05-17 16:40:00 +0200

    convert errno to normal error messages

--- src/apicast-cli/utils/exec.lua
+++ src/apicast-cli/utils/exec.lua
@@ -6,16 +6,9 @@ local typeof = ffi.typeof
 
 ffi.cdef([[
 int execve(const char *filename, const char *argv[], const char *envp[]);
+char *strerror(int errnum);
 int execvp(const char *file, const char *argv[]);
 ]])
-
-inspect = require 'inspect'
-
--- used for no return value, return true for use of assert
-local function retbool(ret)
-  if ret == -1 then return nil, ffi.errno() end
-  return true
-end
 
 local string_array_t = typeof("const char *[?]")
 
@@ -23,7 +16,8 @@ local function execvp(filename, ...)
   local argv = { filename, ... }
   local cargv = string_array_t(#argv + 1, argv)
   cargv[#argv] = nil
-  return retbool(C.execvp(filename, cargv))
+  C.execvp(filename, cargv)
+  error(ffi.string(ffi.C.strerror(ffi.errno())))
 end
 
 return execvp
