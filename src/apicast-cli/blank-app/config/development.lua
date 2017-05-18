b90dd754b06151281d98e7eb6997346bf98599f7
Michal Cichra <michal@cichra.cz>
2017-05-18 15:38:18 +0200

    improve templates to suppload loading files from local filesystem

--- src/apicast-cli/blank-app/config/development.lua
+++ src/apicast-cli/blank-app/config/development.lua
@@ -1,5 +1,12 @@
+local lr_path, lr_cpath, lr_bin = require('luarocks.cfg').package_paths()
+
 return {
   worker_processes = '1',
   master_process = 'off',
   lua_code_cache = 'off',
+  lua_path = "./src/?.lua;./src/?/init.lua;"..lr_path,
+  lua_cpath = lr_cpath,
+  env = {
+    PATH = lr_bin -- this probably also needs to use the previous value
+  }
 }
