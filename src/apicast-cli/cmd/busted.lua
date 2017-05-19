594554e4bf888fa9a97880e7373b69009e7a7625
Michal Cichra <michal@cichra.cz>
2017-05-19 09:48:08 +0200

    remove unnecessary code

--- src/apicast-cli/cmd/busted.lua
+++ src/apicast-cli/cmd/busted.lua
@@ -2,13 +2,7 @@ local exec = require('apicast-cli.utils.exec')
 local utils_path = require "apicast-cli.utils.path"
 local path = require "pl.path"
 
-local run_tests = function (args, _) 
-  if ngx ~= nil then
-    ngx.exit = function()end
-  end
-
-  pcall(require, 'luarocks.loader')
-  
+local run_tests = function (args, _)
   local busted_wrapper = path.join(utils_path.find(), 'apicast-cli', 'busted_wrapper.lua')
   local spec_folder = path.join(path.currentdir(), 'spec')
 
